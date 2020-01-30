bad6938fa78d7ad7d603fb5c813c06604bfc5120
melissay94 <melissay94@yahoo.com>
2020-01-30 12:02:08 -0800

    Kanye gives you wisdom

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
 console.log("👀")
+
+document.addEventListener("DOMContentLoaded", function(){
+    requestKanyeQuote();
+    document.getElementById("kanye-btn").addEventListener("click", requestKanyeQuote);
+    document.getElementById("code-btn").addEventListener("click", requestCodingQuote);
+});
+
+const requestKanyeQuote = function() {
+
+    fetch("https://api.kanye.rest")
+        .then(function(apiData){
+            return apiData.json();
+        })
+        .then(function(jsonData) {
+            displayKnowledge(jsonData.quote);
+        });
+};
+
+const requestCodingQuote = function() {
+    fetch("https://programming-quotes-api.herokuapp.com/quotes/random")
+        .then(function(apiData){
+            return apiData.json();
+        })
+        .then(function(jsonData) {
+            displayKnowledge(`${jsonData.en}\n-${jsonData.author}`);
+        })
+}
+
+const displayKnowledge = function(quote) {
+    document.getElementById("kanye-quote").textContent = quote;
+}
